c577daa6b8ad8fa9f462cf51093919b732300f70
kanakata <patrick37668@gmail.com>
2026-09-15 00:24:13 +0300

    2026-09-14 21:24:12

--- run.php
+++ run.php
@@ -1,6 +1,6 @@
 <?php
 $folder = $argv[1] ?? null;
-
+$file= __DIR__ . '/6637770331783168.wav';
 if ($folder === "git") {
     $commitMsg = date('Y-m-d H:i:s');
 
@@ -9,12 +9,18 @@ if ($folder === "git") {
         $remoteUrl = escapeshellarg($argv[2]);
 
         system("git init && git add . && git commit -m \"{$commitMsg}\" && git remote add origin {$remoteUrl} && git push -u origin master");
+        $psCommand = "powershell -c \"(New-Object Media.SoundPlayer '{$file}').PlaySync()\"";
+        exec($psCommand . " > NUL 2>&1");
     } else {
         system("git init && git add . && git commit -m \"{$commitMsg}\" && git push");
+        $psCommand = "powershell -c \"(New-Object Media.SoundPlayer '{$file}').PlaySync()\"";
+        exec($psCommand . " > NUL 2>&1");
     }
 } else {
     // Determine public root folder
     $targetFolder = empty($folder) ? "public" : escapeshellcmd($folder);
 
     system("php -S localhost:8000 -t " . $targetFolder);
+    $psCommand = "powershell -c \"(New-Object Media.SoundPlayer '{$file}').PlaySync()\"";
+        exec($psCommand . " > NUL 2>&1");
 }
