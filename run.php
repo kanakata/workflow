80af78cc7c06ef74d83b6cbda452c577ae33cabb
kanakata <patrick37668@gmail.com>
2026-09-15 00:41:27 +0300

    2026-09-14 21:41:26

--- run.php
+++ run.php
@@ -1,6 +1,6 @@
 <?php
 $folder = $argv[1] ?? null;
-$file= __DIR__ . '/patoriki_push_tag.mp3';
+$file= __DIR__ . '<div class=""></div>/patoriki_push_tag.mp3';
 if ($folder === "git") {
     $commitMsg = date('Y-m-d H:i:s');
 
@@ -10,13 +10,13 @@ if ($folder === "git") {
         system("git init && git add . && git commit -m \"{$commitMsg}\" && git remote add origin {$remoteUrl} && git push -u origin master");
     } else {
         system("git init && git add . && git commit -m \"{$commitMsg}\" && git push");
-        
     }
+
+    $psCommand = "powershell -c \"(New-Object Media.SoundPlayer '{$file}').PlaySync()\"";
+    exec($psCommand . " > NUL 2>&1");
 } else {
     // Determine public root folder
     $targetFolder = empty($folder) ? "public" : escapeshellcmd($folder);
     system("php -S localhost:8000 -t " . $targetFolder);
 }
 
-$psCommand = "powershell -c \"(New-Object Media.SoundPlayer '{$file}').PlaySync()\"";
-exec($psCommand . " > NUL 2>&1");
